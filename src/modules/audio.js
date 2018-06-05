c4526f324d6419f052b9b5ddd1fb308ec0251e18
Can Serkan UREN <canserkan.uren@gmail.com>
2018-06-05 17:26:10 +0200

    fixing search by words loop

--- src/modules/audio.js
+++ src/modules/audio.js
@@ -1,10 +1,10 @@
 const YoutubeStream = require('youtube-audio-stream');
 const auth = require('../../config/auth.json');
-// const {google} = require('googleapis');
-// let youtube = google.youtube({
-//     version: 'v3',
-//     auth: auth.youtubeToken
-// });
+const {google} = require('googleapis');
+let youtube = google.youtube({
+    version: 'v3',
+    auth: auth.youtubeToken
+});
 
 module.exports = class Audio {
     static match(message) {
@@ -47,27 +47,59 @@ module.exports = class Audio {
     }
 
     static playFromWords(message) {
-        // let voiceChannel = this.currentChannel(message);
-
-        // let args = message.content.split(" ");
+        let args = message.content.split(" ");
         
-        // let query = "";
-        // args.splice(1, 0);
-        // args.forEach(arg => {
-        //     query += arg;
-        // })
+        let query = "";
+        args.splice(0,1);
+        args.forEach(arg => {
+            query += arg + " ";
+        })
 
-        // youtube.search.list({
-        //     part: 'snippet',
-        //     q: query
-        // }, (err, data) => {
-        //     if (err) {
-        //         console.error('Error: ' + err);
-        //     }
-        //     if (data) {
-        //         console.log(data)
-        //     }
-        // });
+        this.searchVideoFromYoutube(query).then(data => {
+            let videoUrl = "https://www.youtube.com/watch?v=" + data.id.videoId;
+            let voiceChannel = this.currentChannel(message);
+            console.log(message.author.username != "music-bot");
+            console.log(message.author.username);
+            if (videoUrl) {
+                if(message.author.username != "music-bot") {
+                    voiceChannel
+                        .join()
+                        .then(connection => {
+                            try {
+                                message.channel.send(":fast_forward: Now Playing : " + data.snippet.title);
+                                let stream = YoutubeStream(videoUrl);
+                                connection.playStream(stream).on('end', () => {
+                                    connection.disconnect();
+                                })
+                            } catch (e) {
+                                message.channel.send("WOLA YA EU UN PROBLEME JSE PO C KOA");
+                                console.log(e);
+                            }
+                        })
+                }
+            } else {
+                message.channel.send("ENCULE DTA MERE T'AS PAS RENTRE D'URL DE VIDEO OU WOLA TU SAIS PAS ECRIRE");
+                console.log("No url has been given to me");
+            }
+        }, err => {
+            message.channel.send(err);
+        })
+    }
+
+    static searchVideoFromYoutube(videoName) {
+        return new Promise((resolve, reject) => {
+            youtube.search.list({
+                part: 'snippet',
+                q: videoName
+            }, (err, data) => {
+                if (err) {
+                    reject("WOLA LA VIDEO EST PAS DISPO");
+                }
+                if (data) {
+                    resolve(data.data.items[0]);
+                }
+            });
+        })
     }
 
     static currentChannel(message) {
